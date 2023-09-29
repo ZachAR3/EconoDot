0d37b8e08f84624ba08235247eb33b0d9ff19b59
ZachAR3 <zacharyschwentor@gmail.com>
2023-09-30 00:30:54 +0530

    Added auto refreshing to graph previews

--- Scripts/GraphsPreview.gd
+++ Scripts/GraphsPreview.gd
@@ -5,15 +5,28 @@ extends ItemListBase
 @export_dir var premade_graphs : String
 @export var graph_preview_scene : PackedScene
 
+var previews := []
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	refresh()
+
+
+func refresh():
 	var graphs_directory = DirAccess.open(premade_graphs)
-	# TODO #17 (Fix crappy path grabbing)
 	for graph_file in graphs_directory.get_files():
+		# TODO #17 (Fix crappy path grabbing)
+		var graph_resource = FileManager.load_data(graphs_directory.get_current_dir() + "/" + graph_file)
+		
+		if previews.has(graph_resource.points):
+			continue
+			
 		var graph = graph_preview_scene.instantiate()
-		graph.graph = FileManager.load_data(graphs_directory.get_current_dir() + "/" + graph_file)
+		graph.graph = graph_resource
+		
 		add_item(graph.graph.name, graph)
+		previews.append(graph_resource.points)
 
 
 func _double_clicked(index : int):
