3f575ffba97f2f4b1fc0e4e7ffe2625cfbb3c850
ZachAR3 <zacharyschwentor@gmail.com>
2023-10-01 23:29:23 +0530

    closes #35; fixed graph saving and imports due to incorrectly using global pos for handles and not local + min x and y size

--- Scripts/PreviewMaker.gd
+++ Scripts/PreviewMaker.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 var points
-var remapped_points
+var final_points
 var graph
 var drawing_size
 var redraw = false
@@ -15,16 +15,19 @@ var last_call_time := 0.0
 
 
 func _ready():
-	preview.get_parent().draw.connect(remap_points)
+	preview.get_parent().draw.connect(size_updated)
 
 
 func get_preview():
 	var graph_curve = Curve2D.new()
 	for point in graph.points:
 		var handles_enabled = graph.points[point][2]
+		
 		var handle_1 = int(handles_enabled) * graph.points[point][0]
 		var handle_2 = int(handles_enabled) * graph.points[point][1]
+		
 		graph_curve.add_point(point, handle_1, handle_2)
+	
 	points = graph_curve.tessellate(5, 2)
 	get_drawing_size()
 	if len(points) > 1:
@@ -50,7 +53,11 @@ func get_drawing_size():
 	return drawing_size
 
 
-func remap_points():
+func size_updated():
+	final_points = await remap_points(points)
+
+
+func remap_points(original_points = null) -> Array:
 	# Infinite loop prevention
 	var time = Time.get_ticks_msec()
 	if time - last_call_time < 5:
@@ -72,15 +79,15 @@ func remap_points():
 	preview.custom_minimum_size = new_size
 	
 #	Fit the points into our new mapped and aspect correct container 
-	remapped_points = points.duplicate()
+	var remapped_points = original_points.duplicate()
 	for point in range(len(remapped_points)):
 		remapped_points[point].x = remap(remapped_points[point].x, drawing_size[1], max(drawing_size[0], graph.width), padding, new_size.x - padding)
 		remapped_points[point].y = remap(remapped_points[point].y, drawing_size[3], max(drawing_size[2], graph.width), padding, new_size.y - padding)
 	last_call_time = time
+	return remapped_points
 
 
 func _draw():
-	if remapped_points:
-		print(graph.name, remapped_points)
-		draw_polyline(remapped_points, graph.color, graph.width, true)
+	if final_points:
+		draw_polyline(final_points, graph.color, graph.width, true)
 
--- Scripts/SaveManager.gd
+++ Scripts/SaveManager.gd
@@ -3,11 +3,15 @@ extends Resource
 class_name SaveManager
 
 
-static func save_graph(graph, location) -> void:
+static func save_graph(graph, path : String) -> void:
 	var data = graph.resources
 	for point in graph.points:
-		data.points[point.global_position] = [point.handles[0].global_position, point.handles[1].global_position, point.handles_enabled]
-	var file = FileAccess.open(location, FileAccess.WRITE)
+		if path.get_extension() != ".ed":
+			path = path.get_basename() + ".ed"
+		
+		# Vec 2 position = [handles 1, handles 2, handles enabled]
+		data.points[point.global_position] = [point.handles[0].position, point.handles[1].position, point.handles_enabled]
+	var file = FileAccess.open(path, FileAccess.WRITE)
 	file.store_string(var_to_str(data))
 
 
